cd52f18ae25bcf7cb661f9b1aa443c6affe48516
Shawn Cicoria <github@cicoria.com>
2021-12-29 21:13:50 -0500

    updates

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,44 +1,34 @@
 use std::collections::HashMap;
-use std::env;
-// https://www.freecodecamp.org/news/how-to-build-a-to-do-app-with-rust/
+use std::str::FromStr;
 
-// https://stackoverflow.com/a/65308211/140618
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    
     let mut todo = Todo {
         map: HashMap::new(),
     };
-
     if action == "add" {
         todo.insert(item);
         match todo.save() {
             Ok(_) => println!("todo saved"),
             Err(why) => println!("An error occurred: {}", why),
         }
-    }
-    
+    };
+
+    // My Stuff
     let current_dir = get_dir().unwrap();
-    // {
-    //     Ok(r) => println!("got it... {}", r),
-    //     Err(why) => println!("An error occurred: {}", why),
-    // };
-    
-    println!("{:?}, {:?}", action, current_dir);
+    println!("current path is: {:?}", current_dir);
 }
 
 fn get_dir() -> Result<String, std::io::Error> {
-    let path =  match env::current_dir() {
+    let path =  match std::env::current_dir() {
         Ok(path) => path,
         Err(e) => return Err(e),
     };
     Ok(path.display().to_string())
 }
-
-
 
 struct Todo {
     // use rust built in HashMap to store key - val pairs
@@ -46,11 +36,29 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        std::io::Read::read_to_string(&mut f, &mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+    
     fn insert(&mut self, key: String) {
         // insert a new item into our map.
-        // we pass true as value
+        // we pass true as value.
         self.map.insert(key, true);
     }
+
     fn save(self) -> Result<(), std::io::Error> {
         let mut content = String::new();
         for (k, v) in self.map {
@@ -60,4 +68,3 @@ impl Todo {
         std::fs::write("db.txt", content)
     }
 }
-
